96c45c85c3699c8c1ed84c28c6532252e0d119f4
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-02-09 13:10:03 -0800

    Add functionality to calculate and save results to csv

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 const fileSelector = document.querySelector('#file-selector');
 const button = document.querySelector('#button');
+const repNumberInput = document.querySelector('#noOfReps');
+let repNumber;
 let fileContent;
 let fileReadingFinished = false;
 
@@ -18,12 +20,11 @@ fileSelector.addEventListener('change', event => {
   // console.log(fileContent);
 })
 
-const repNumberInput = document.querySelector('#noOfReps');
-const repNumber = Number(repNumberInput.value);
-let result = [];
+
 
 function convertToArray(str) {
   let statePopulation = str.split('\n').filter(item => item.length !== 0);
+  statePopulation.shift();
   statePopulation = statePopulation.sort();
   let result = [];//why using statePopulation would not work?
   statePopulation.forEach(item => {
@@ -36,82 +37,76 @@ function convertToArray(str) {
   return result;
 }
 
+
+function calculate(array, num) {
+  let totalPopulation = 0;
+  console.log(array);
+  array.forEach(item => totalPopulation = totalPopulation + item[1]);
+  // console.log(totalPopulation);
+  let avg = Math.round((totalPopulation / num));
+  
+  let finalList =[]; //if not initiated would error 'not defined'
+  let repSum = 0;
+  let secondList = [];
+  array.forEach(item => {
+    let newItem = [item[0]];
+    let anotherItem = [item[1] % avg];
+    anotherItem.push(item[0]);
+    let repNumber = Math.floor((item[1] / avg));
+    repSum += repNumber;
+    newItem.push(repNumber);
+    finalList.push(newItem);
+    secondList.push(anotherItem);
+  })
+
+  if (repSum < num) {
+    console.log(`${repSum}, ${num}`);
+    let testArray = secondList.sort((a, b) => b[0] - a[0]);//!!!
+
+    for (let i = 0; i < (num - repSum); i++) {
+      let theItem = finalList.find(item => item[0] === secondList[i][1]);
+      theItem[1]++;
+    }
+  }
+  return finalList;
+}
+
+
+
 button.addEventListener('click', event => {
   console.error('hiiiii');//why not displaying???
   if (!fileReadingFinished) console.log("unfinished, try later!");//same!
   else {
-    // console.log('ready');
+    console.log('ready');
     // console.log(fileContent);
     statePopulation = convertToArray(fileContent);
-    console.log(statePopulation);
-    let csvContent = "data:text/csv;charset=utf-8," 
-    + statePopulation.map(e => e.join(",")).join("\n");
-
-    let encodedUri = encodeURI(csvContent);
-    window.open(encodedUri);
+    // console.log(statePopulation);
+    repNumber = Number(repNumberInput.value) || 435;
+    // console.log(repNumber);
+    let finalList = calculate(statePopulation, repNumber);
+    displayResult(finalList);
+    saveAsCsv(finalList);
+  
   }
-
-  
-  
-  //   reader.addEventListener('load', (event) => {
-  //     // console.log(reader.readyState);
-  //     let result = event.target.result;
-      // statePopulation = result.split(/(?:\n)+/);
-    
-  //     // console.log(statePopulation);
-  
-  //     let updatedArray = [];
-  //     statePopulation.forEach(item => {
-  //       item = newString;
-  //       item = item.split(',');
-  //       item[1] = +item[1];
-  //       updatedArray.push(item);
-  
-  //       // if (item[0].length !== 0) updatedArray.push(item);
-  //     })
-  //     // console.log(statePopulation);
-  //     statePopulation = updatedArray;
-  //     console.log(statePopulation);
-  //     //what I just learnt: functionName(num = 435) - default value
-  //     let noOfReps = repNumber || 435;
-  // console.log(noOfReps);
-  // let totalPopulation = 0;
-  // console.log(totalPopulation);
-  // console.log(statePopulation);
-  // statePopulation.forEach(item => {
-  //   totalPopulation += item[1];
-  //   console.log(totalPopulation);
-  // })
-  // console.log(totalPopulation);
-  // let averagePopPerRep = Math.round((totalPopulation / noOfReps));
-  // console.log(averagePopPerRep);
-  
-  // statePopulation.forEach(item => {
-  //   result.push(`${item[0]},${Math.floor((item[1]/averagePopPerRep))}`);
-  //   item.push(Math.floor((item[1]/averagePopPerRep)));
-  //   console.log(`${item[1]/averagePopPerRep}`);
-  //   item.push(Math.floor((item[1]/averagePopPerRep)));
-  //   item.push(item[1]%averagePopPerRep);
-  // })
-  // console.log(statePopulation[0]);
-  // displayResult(statePopulation);
-
-  // let csvContent = "data:text/csv;charset=utf-8," 
-  //   + statePopulation.map(e => e.join(",")).join("\n");
-
-  // var encodedUri = encodeURI(csvContent);
-  // window.open(encodedUri);
-    // }
-    // )
-  // });
 })
 
 
+//FIXME: flashes quickly on screen then disappears!!!
 function displayResult(array) {
   const resultContainer = document.querySelector('#resultList');
+  // resultContainer.textContent = 'testing';
   array.forEach(item => {
     let list = document.createElement('li');
     list.textContent = `${item[0]}, ${item[1]}`;
     resultContainer.appendChild(list);
   })
 }
+
+function saveAsCsv(array) {
+  let csvContent = "data:text/csv;charset=utf-8," 
+  + array.map(e => e.join(",")).join("\n");
+  
+  var encodedUri = encodeURI(csvContent);
+  window.open(encodedUri);
+}
+
